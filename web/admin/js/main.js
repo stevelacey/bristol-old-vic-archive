8a9a76017a047ce502a069d72cda2ad089ee09db
Steve Lacey <steve@stevelacey.net>
2011-02-12 00:38:31 +0000

    Added fancybox.

---
+++ web/admin/js/main.js
@@ -0,0 +1,18 @@
+$(function() {
+  fancybox();
+});
+
+function fancybox() {
+  $("a[href $= '.jpg'], a[href $= '.jpeg'], a[href $= '.png'], a[href $= '.gif']").fancybox({
+    'zoomOpacity': true,
+    'overlayShow': false,
+    'zoomSpeedIn': 500,
+    'zoomSpeedOut': 500,
+    'transitionIn': 'elastic',
+    'transitionOut': 'elastic',
+    'easingIn': 'easeOutBack',
+    'easingOut': 'easeInBack',
+    'centerOnScroll': true,
+    'hideOnContentClick': false
+  });
+}
