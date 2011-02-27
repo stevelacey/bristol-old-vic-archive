2682755c990242a6b8e3bffcd3323c63b207a73e
Steve Lacey <steve@stevelacey.net>
2011-02-27 14:08:40 +0000

    Moved new button to top of list interfaces.

--- data/generator/sfDoctrineModule/admin/template/templates/indexSuccess.php
+++ data/generator/sfDoctrineModule/admin/template/templates/indexSuccess.php
@@ -8,6 +8,9 @@
 
   <div id="sf_admin_header">
     [?php include_partial('<?php echo $this->getModuleName() ?>/list_header', array('pager' => $pager)) ?]
+    <ul class="sf_admin_actions">
+      [?php include_partial('<?php echo $this->getModuleName() ?>/list_actions', array('helper' => $helper)) ?]
+    </ul>
   </div>
 
 <?php if ($this->configuration->hasFilterForm()): ?>
@@ -23,7 +26,6 @@
     [?php include_partial('<?php echo $this->getModuleName() ?>/list', array('pager' => $pager, 'sort' => $sort, 'helper' => $helper)) ?]
     <ul class="sf_admin_actions">
       [?php include_partial('<?php echo $this->getModuleName() ?>/list_batch_actions', array('helper' => $helper)) ?]
-      [?php include_partial('<?php echo $this->getModuleName() ?>/list_actions', array('helper' => $helper)) ?]
     </ul>
 <?php if ($this->configuration->getValue('list.batch_actions')): ?>
     </form>
