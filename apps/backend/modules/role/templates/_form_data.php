17440879d9f6833771ad6d279164ac4d9fc1fd48
Steve Lacey <steve@stevelacey.net>
2011-03-09 14:42:49 +0000

    Implemented role form data.

--- apps/backend/modules/role/templates/_form_data.php
+++ apps/backend/modules/role/templates/_form_data.php
@@ -2,17 +2,19 @@
     <h2>Staff</h2>
     <?php if($role->getStaff()->count()) : ?>
       <ul>
-        <li>
-          <?php echo link_to($staff, 'staff_edit', $staff) ?>
-          <ul>
-            <?php foreach($staff->getProductions() as $production) : ?>
-              <li>
-                <h3><?php echo link_to($production, 'production_edit', $production) ?></h3>
-                <time><?php echo $production->getStartDate(sfConfig::get('app_data_date_format')) ?></time>
-              </li>
-            <?php endforeach ?>
-          </ul>
-        </li>
+        <?php foreach($role->getStaff() as $staff) : ?>
+          <li>
+            <h3><?php echo link_to($staff, 'staff_edit', $staff) ?></h3>
+            <ul>
+              <?php foreach($staff->getProductions() as $production) : ?>
+                <li>
+                  <?php echo link_to($production, 'production_edit', $production) ?>
+                  <time><?php echo $production->getStartDate(sfConfig::get('app_data_date_format')) ?></time>
+                </li>
+              <?php endforeach ?>
+            </ul>
+          </li>
+        <?php endforeach ?>
       </ul>
     <?php else : ?>
       <p>No associated staff.</p>
