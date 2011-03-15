9f78d3ca1cd5b9d9cd362b67f674053c338d6c8b
Steve Lacey <steve@stevelacey.net>
2011-03-15 19:06:57 +0000

    Exposed super admin functionality for user administration.

---
+++ apps/backend/modules/sfGuardAuth/templates/secureSuccess.php
@@ -0,0 +1,6 @@
+<?php use_helper('I18N') ?>
+
+<div id="sf_admin_container">
+  <h1><?php echo __('Access Denied', null, 'sf_guard') ?></h1>
+  <p><?php echo __('Oops! The page you asked for is secure and you do not have the required credentials.', null, 'sf_guard') ?></p>
+</div>
--- lib/form/doctrine/sfDoctrineGuardPlugin/sfGuardUserForm.class.php
+++ lib/form/doctrine/sfDoctrineGuardPlugin/sfGuardUserForm.class.php
@@ -8,9 +8,9 @@
  * @author     Steve Lacey
  * @version    SVN: $Id: sfDoctrinePluginFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class sfGuardUserForm extends PluginsfGuardUserForm
-{
-  public function configure()
-  {
+class sfGuardUserForm extends sfGuardUserAdminForm {
+  public function configure() {
+    $this->widgetSchema->setHelp('is_super_admin', 'Only super administrators have access to the user module, and thus can add/edit/delete users.');
+    $this->widgetSchema->setHelp('is_active', 'Only active users can login to the archive, this option is available if you need to disable a user\'s account without deleting them from the system.');
   }
 }
